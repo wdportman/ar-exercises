14aafa36e339a7bdfd2447f26fc7afa57c9487ee
Will <wdportman@gmail.com>
2020-11-10 10:50:01 -0800

    Complete exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,8 +1,16 @@
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
+require 'pp'
 
 puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+puts Store.count
+# pp Store.all
+# pp Store.find_by(id: 3)
+# pp @store3
